ede95a173e408388c645fb11c02d4314a37c472c
Gabriel Lisboa <gabriellisboa.rx@gmail.com>
2023-06-03 16:17:50 -0300

    fix: Build code with ESbuild

---
+++ build.js
@@ -0,0 +1,22 @@
+const { build } = require("esbuild");
+const { dependencies, peerDependencies } = require("./package.json");
+const { Generator } = require("npm-dts");
+
+new Generator({
+  entry: "src/index.ts",
+  output: "dist/index.d.ts",
+}).generate();
+
+const sharedConfig = {
+  entryPoints: ["src/index.ts"],
+  bundle: true,
+  minify: true,
+  external: Object.keys(dependencies).concat(Object.keys(peerDependencies)),
+};
+
+build({
+  ...sharedConfig,
+  outfile: "dist/index.esm.js",
+  platform: "neutral", // for ESM
+  format: "esm",
+});
